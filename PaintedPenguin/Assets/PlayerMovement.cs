a44264f79fdf16139da6034155846d05e3929827
Micheal Quentin <mquenti3@gmail.com>
2019-07-16 14:02:39 -0500

    Added Colliders and Player Physics

---
+++ PaintedPenguin/Assets/PlayerMovement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    public float velocity = 10f;
+
+    void Start()
+    {
+        Physics.gravity = Vector2.zero;
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        if (transform.position.y > 0.125)
+        {
+            //Physics.gravity = new Vector2(0, -1f);
+        }
+            else
+        {
+            Physics.gravity = Vector2.zero;
+        }
+
+        if (Input.GetKeyDown("up"))
+        {
+            rb.velocity = Vector2.up * 5;
+        }
+
+
+    }
+}
---
+++ PaintedPenguin/Assets/StartButton.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartButton : MonoBehaviour
+{
+    void TaskOnClick()
+    {
+        Debug.Log("Start Button Clicked");
+    }
+}
