4afd7f8a597a2aaef475abdb8f8b5fb4d7d3c1d6
Micheal Quentin <mquenti3@gmail.com>
2019-12-17 23:02:48 -0600

    Added Loading Screen
    This should hopefully prevent previous issues of when sounds first play, it freezes the game, as well as the newer issue of the game crashing entirely when playing sounds.

---
+++ PaintedPenguin/Assets/LevelLoader.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LevelLoader : MonoBehaviour
+{
+    void Start()
+    {
+        LoadLevel(1);
+    }
+    
+    public Slider slider;
+
+    public void LoadLevel(int sceneIndex)
+    {
+        StartCoroutine(LoadAsynchronously(sceneIndex));
+    }
+
+    IEnumerator LoadAsynchronously(int sceneIndex)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
+
+        while (!operation.isDone)
+        {
+            float progress = Mathf.Clamp01(operation.progress / 0.9f);
+            slider.value = progress;
+
+            yield return null;
+        }
+    }
+}
